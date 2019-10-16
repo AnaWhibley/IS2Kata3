98c28dfb986d6735955b4bb5b76679f7a038499c
Usuario <dscbvdfv@sdg.es>
2019-10-16 09:43:50 +0100

    Version 2 finished

--- src/is2kata3/IS2Kata3.java
+++ src/is2kata3/IS2Kata3.java
@@ -4,7 +4,17 @@ public class IS2Kata3 {
 
     
     public static void main(String[] args) {
-        HistogramDisplay display = new HistogramDisplay();
+        Histogram<String> histogram = new Histogram<>();
+        
+        histogram.increment("gmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        
+        HistogramDisplay display = new HistogramDisplay(histogram);
         display.execute();
     }
     
