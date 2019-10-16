f55aec259dbe95278cd90bc9723c046007096625
Usuario <dscbvdfv@sdg.es>
2019-10-16 09:34:16 +0100

    Added Histogram class

--- src/is2kata3/IS2Kata3.java
+++ src/is2kata3/IS2Kata3.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package is2kata3;
 
-/**
- *
- * @author Usuario
- */
 public class IS2Kata3 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        HistogramDisplay display = new HistogramDisplay();
+        display.execute();
     }
     
 }
